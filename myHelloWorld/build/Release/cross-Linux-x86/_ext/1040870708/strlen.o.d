d1b9a334adda0f6ad63685acfc9ab152e4c107ae
Chris Register <vbwizard@live.com>
2017-02-11 09:48:06 -0500

    Updated to work in git

--- myHelloWorld/build/Release/cross-Linux-x86/_ext/1040870708/strlen.o.d
+++
@@ -1,2 +0,0 @@
-build/Release/cross-Linux-x86/_ext/1040870708/strlen.o: \
- ../chrisOS/src/strings/strlen.c
--- myHelloWorld/build/Release/cross-Linux-x86/_ext/1643979071/memcpy.o.d
+++
@@ -1,9 +0,0 @@
-build/Release/cross-Linux-x86/_ext/1643979071/memcpy.o: \
- ../chrisOS/src/memcpy.c ../chrisOS/include/chrisos.h \
- ../chrisOS/include/i386/cpu.h ../chrisOS/include/i386/bits/types.h
-
-../chrisOS/include/chrisos.h:
-
-../chrisOS/include/i386/cpu.h:
-
-../chrisOS/include/i386/bits/types.h:
